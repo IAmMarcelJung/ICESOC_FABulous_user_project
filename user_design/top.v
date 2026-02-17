3681b9b391f4c7b0ef16c7afef6ee99697625e1f
Marcel Jung <MarcelJung97@web.de>
2026-02-17 17:01:33 +0100

    refactor: replace tabs by whitespaces in the top module and adapt the module to the ICESOC eFPGA

--- user_design/top.v
+++ user_design/top.v
@@ -1,17 +1,18 @@
 module top(input wire clk, input wire [9:0] io_in, output wire [9:0] io_out, io_oeb);
-	wire rst = io_in[0];
-	wire en = io_in[1];
-	reg [15:0] ctr;
+    wire rst = io_in[0];
+    wire en = io_in[1];
+    reg [15:0] ctr;
 
-	always @(posedge clk)
-		if (en)
-			if (rst)
-				ctr <= 0;
-			else
-				ctr <= ctr + 1'b1;
-		else
-			ctr <= ctr;
+    always @(posedge clk)
+        if (en)
+            if (rst)
+                ctr <= 0;
+            else
+                ctr <= ctr + 1'b1;
+        else
+            ctr <= ctr;
 
-	assign io_out = {12'b0, ctr}; // pass thru reset for debugging
-	assign io_oeb = 28'b1;
+    // assign io_out =  ctr[15:8];
+    assign io_out =  ctr[9:0];
+    assign io_oeb = 10'h1;
 endmodule
